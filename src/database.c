924630263e2dfc9fbabef462e2939d53d3e15755
saljs <salskare@gmail.com>
2015-11-04 14:52:33 -0600

    Database disk functions complete, some changes to struture implememtations

--- src/database.c
+++ src/database.c
@@ -3,16 +3,31 @@
 memList* newMemory(memList* start)
 {
     memory* newMem = malloc(sizeof(memory));
-    newMem->uuid = start->mem->uuid + 1;
+    if(newMem == NULL)
+    {
+        return NULL;
+    }
+    if(start != NULL)
+    {
+        newMem->uuid = start->mem->uuid + 1;
+    }
+    else
+    {
+        newMem->uuid = 0;
+    }
     for(int i = 0; i < NUMINPUTS; i++)
     {
         newMem->inputs[i] = NULL;
     }
     for(int i = 0; i < NUMOUTPUTS; i++)
     {
-        newMem->actions = NULL;
+        newMem->actions[i] = NULL;
     }
     memList* newStart = malloc(sizeof(memList));
+    if(newStart == NULL)
+    {
+        return NULL;
+    }
     newStart->mem = newMem;
     newStart->next = start;
     return newStart;
@@ -29,36 +44,323 @@ void addAction(memList* current, action* toAdd, int index)
     current->actions[index] = toAdd;
 }
 
-memList* loadDatabase(const char* filename)
+memList* loadDatabase(const char* basedir)
 {
-    FILE* database = fopen(filename, "r");
     memList* last = NULL;
     memList* next;
-    memory* newMem = malloc(sizeof(memory));
-    while(fread(newMem, sizeof(memory), 1, database) > 0)
+
+    DIR* base = opendir(basedir);
+    if(base == NULL)
     {
-        next = malloc(sizeof(memList));
+        return = newMemory(NULL);
+    }
+    struct dirent* uuid_dir;
+    while((uuid_dir = readdir(base)))
+    {
+        if(strcmp(uuid_dir->d_name, "."))
+        {
+            continue;
+        }
+        if(strcmp(uuid_dir->d_name, ".."))
+        {
+            continue;
+        }
+        if(uuid_dir->d_type != DT_DIR)
+        {
+            continue;
+        }
+        memory* newMem = malloc(sizeof(memory));
+        if(newMem == NULL)
+        {
+            return NULL;
+        }
+        newMem->uuid = atol(uuid_dir->d_name);
+        char* filename = malloc(1000);
+        if(filename == NULL)
+        {
+            return NULL;
+        }
+        sprtinf(filename, "%s/%s", basedir, uuid_dir->d_name);
+        DIR* memdir = opendir(filename);
+        if(memdir == NULL)
+        {
+            return NULL;
+        }
+        free(filename);
+        struct dirent* memfiles;
+        //loop through save files for inputs/action
+        while((memfiles = readdir(memdir)))
+        {
+            if(strcmp(memfiles->d_name, "."))
+            {
+                continue;
+            }
+            if(strcmp(memfiles->d_name, ".."))
+            {
+                continue;
+            }
+            FILE* savefile;
+            char* savename = malloc(1000);
+            if(savename == NULL)
+            {
+                return NULL;
+            }
+            sprintf(savename, "%s/%s/%s", basedir, uuid_dir->d_name, memfiles->d_name);
+            savefile = fopen(savename, "r");
+            if(savefile == NULL)
+            {
+                continue;
+            }
+            free(savename);
+            //parse save file
+            magic magicBuff;
+            if(fread(&magicBuff, sizeof(magic), 1, savefile) > 0)
+            {
+                if(magicBuff.magicBits != MAGIC_NUM)
+                {
+                    continue;
+                }
+                else if(magicBuff.type == INPUT)
+                {
+                    input* newInput = malloc(sizeof(input));
+                    if(newInput == NULL)
+                    {
+                        return NULL
+                    }
+                    input* lastInput = NULL;
+                    newMem->inputs[magicBuff.index] = newInput;
+                    //loop through to rebuild linked list
+                    while(fread(&newInput->link, sizeof(long int), 1, savefile) == sizeof(long int))
+                    {
+                        lastInput = newInput;
+                        newInput = malloc(sizeof(input));
+                        if(newInput == NULL)
+                        {
+                            return NULL;
+                        }
+                        lastInput->next = newInput;
+                        if(fread(&newInput->link, sizeof(long int), 1, savefile) < sizeof(long int))
+                        {
+                            return NULL;
+                        }
+                        if(fread(&newInput->confidence, sizeof(float), 1, savefile) < sizeof(float))
+                        {
+                            return NULL;
+                        }
+                        if(fread(&newInput->dataSize, sizeof(size_t), 1, savefile) < sizeof(size_t))
+                        {
+                            return NULL;
+                        }
+                        newInput->data = malloc(newInput->dataSize);
+                        if(newInput->data == NULL)
+                        {
+                            return NULL;
+                        }
+                        if(fread(newInput->data, newInpu->dataSize, 1, savefile) < newInput->dataSize)
+                        {
+                            return NULL;
+                        }
+
+                        lastInput = newInput;
+                        newInput = malloc(sizeof(input));
+                        if(newInput == NULL)
+                        {
+                            return NULL;
+                        }
+                        lastInput->next = newInput;
+
+                    }
+                    newInput->next = NULL;
+                }
+                else if(magicBuff.type == OUTPUT)
+                {
+                    action* newAction = malloc(sizeof(action));
+                    if(newAction == NULL)
+                    {
+                        return NULL;
+                    }
+                    action* lastAction = NULL;
+                    newMem->actions[magicBuff.index] = newAction;
+                    //loop through to rebuild linked list
+                    while(fread(&newAction->link, sizeof(long int), 1, savefile) == sizeof(long int))
+                    {
+                        lastAction = newAction;
+                        newAction = malloc(sizeof(input));
+                        if(newAction == NULL)
+                        {
+                            return NULL;
+                        }
+                        lastAction->next = newAction;
+                        if(fread(&newAction->link, sizeof(long int), 1, savefile) < sizeof(long int))
+                        {
+                            return NULL;
+                        }
+                        if(fread(&newAction->confidence, sizeof(float), 1, savefile) < sizeof(float))
+                        {
+                            return NULL;
+                        }
+                        if(fread(&newAction->dataSize, sizeof(size_t), 1, savefile) < sizeof(size_t))
+                        {
+                            return NULL;
+                        }
+                        newAction->data = malloc(newAction->dataSize);
+                        if(newAction->data == NULL)
+                        {
+                            return NULL;
+                        }
+                        if(fread(newAction->data, newInpu->dataSize, 1, savefile) < newAction->dataSize)
+                        {
+                            return NULL;
+                        }
+
+                        lastAction = newAction;
+                        newAction = malloc(sizeof(input));
+                        if(newAction == NULL)
+                        {
+                            return NULL;
+                        }
+                        lastAction->next = newAction;
+
+                    }
+                    newInput->next = NULL;
+                }
+            }
+            fclose(savefile);
+        }
+        closedir(memdir);
+        next = malloc(sizeof(memlist));
+        if(next == NULL)
+        {
+            return NULL;
+        }
         next->mem = newMem;
         next->next = last;
         last = next;
-        newMem = malloc(sizeof(memory));
     }
-    fclose(database);
+    closedir(base);
     return next;
 }
 
-int saveDatabase(const char* filename, memList* start)
+int saveDatabase(const char* basedir, memList* start)
 {
-    FILE* database = fopen(filename, "w");
     memList* parser = start;
+
+    if(!mkdir(basedir, 0700))
+    {
+        if(errno != EEXIST)
+        {
+            return -2;
+        }
+    }
     while(parser != NULL)
     {
-        if(fwrite(parser->mem, sizeof(memory), 1, database) == 0)
+        char* dirname = malloc(1000);
+        if(dirname == NULL)
         {
             return -1;
         }
-        parser = parser->next;
+        sprintf(dirname, "%s/%s", basedir, parser->mem->uuid);
+        if(!mkdir(dirname, 0700))
+        {
+            if(errno != EEXIST)
+            {
+                return -2;
+            }
+        }
+        free(dirname);
+        for(int i = 0; i < NUMINPUTS; i++)
+        {
+            char* filename = malloc(1000);
+            if(filename == NULL)
+            {
+                return NULL;
+            }
+            sprintf(filename, "%s/%s/I%d", basedir, parser->mem->uuid, i);
+            FILE* inputDump = fopen(filename, "w") 
+            if(inputDump == NULL)
+            {
+                return -2;
+            }
+            magic inputOps;
+            inputOps.type = INPUT;
+            inputOps.index = i;
+            inputOps.magicBits = MAGIC_NUM;
+            if(fwrite(&inputOps, sizeof(magic), 1, inputDump) < sizeof(magic))
+            {
+                return -2;
+            }
+            
+            input* nextInput = parser->mem->inputs[i];
+            while(nextInput != NULL)
+            {
+                if(fwrite(&nextInput->link, sizeof(long int), 1, inputDump) < sizeof(long int))
+                {
+                    return -2;
+                }
+                if(fwrite(&nextInput->confidence, sizeof(float), 1, inputDump) < sizeof(float))
+                {
+                    return -2;
+                }
+                if(fwrite(&nextInput->dataSize, sizeof(size_t), 1, inputDump) < sizeof(size_t))
+                {
+                    return -2;
+                }
+                if(fwrite(nextInput->data, nextInput->dataSize, 1, inputDump) < nextInput->dataSize)
+                {
+                    return -2;
+                }
+                input* tmp = nextInput->next;
+                nextInput = tmp;
+            }
+        }
+        for(int i = 0; i < NUMOUTPUTS; i++)
+        {
+            char* filename = malloc(1000);
+            if(filename == NULL)
+            {
+                return NULL;
+            }
+            sprintf(filename, "%s/%s/O%d", basedir, parser->mem->uuid, i);
+            FILE* actionDump = fopen(filename, "w") 
+            if(actionDump == NULL)
+            {
+                return -2;
+            }
+            magic actionOps;
+            actionOps.type = INPUT;
+            actionOps.index = i;
+            actionOps.magicBits = MAGIC_NUM;
+            if(fwrite(&actionOps, sizeof(magic), 1, actionDump) < sizeof(magic))
+            {
+                return -2;
+            }
+            
+            input* nextAction = parser->mem->actions[i];
+            while(nextAction != NULL)
+            {
+                if(fwrite(&nextAction->link, sizeof(long int), 1, actionDump) < sizeof(long int))
+                {
+                    return -2;
+                }
+                if(fwrite(&nextAction->confidence, sizeof(float), 1, actionDump) < sizeof(float))
+                {
+                    return -2;
+                }
+                if(fwrite(&nextAction->dataSize, sizeof(size_t), 1, actionDump) < sizeof(size_t))
+                {
+                    return -2;
+                }
+                if(fwrite(nextAction->data, nextInput->dataSize, 1, actionDump) < nextAction->dataSize)
+                {
+                    return -2;
+                }
+                input* tmp = nextAction->next;
+                nextAction = tmp;
+            }
+        }
+        memList* tmp = parser->next;
+        parser = tmp;
     }
-    fclose(database);
-    return 0;
 }
+
+int freeMem(memList* start)
--- src/database.h
+++ src/database.h
@@ -1,15 +1,23 @@
-
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 #include "inputs.h"
 #include "outputs.h"
 
 #ifndef DATABASE_H_
 #define DATABASE_H_
 
+#define MAGIC_NUM 0xDEADF113
+#define INPUT 0
+#define OUTPUT 1
+
 typedef struct input input;
 typedef struct action action;
 struct input
 {
     char* data;
+    size_t dataSize;
     long int link;
     float confidence;
     input* next;
@@ -17,13 +25,14 @@ struct input
 struct action
 {
     char* data;
+    size_t dataSize;
     long int link;
     float confidence;
     action* next;
 };
 typedef struct 
 {
-    long int uuid;
+    long uuid;
     input* inputs[NUMINPUTS];
     action* actions[NUMOUTPUTS];
 } memory;
@@ -33,6 +42,13 @@ struct memList
     memory* mem;
     memList* next;
 };
+typedef struct
+{
+    unsigned int magicBits;
+    int type;
+    int index;
+} magic;
+
 memList* newMemory(memList* start);
 void addInput(memList* current, input* toAdd, int index);
 void addAction(memList* current, action* toAdd, int index);
